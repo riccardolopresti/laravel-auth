9b7a8ae30679a2157a3fdf6e774c82872b964e04
RiccardoLoPresti <riccardolopresti35@gmail.com>
2023-01-17 16:16:38 +0100

    [add] header

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.guest')
 
 @section('content')
+
+@include('layouts.partials.header')
+
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            HOME
+        <div class="col-md-8 pt-5">
+            <h1>HOME</h1>
         </div>
     </div>
 </div>
