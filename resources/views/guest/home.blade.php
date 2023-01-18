91ace3a5144b1b28dac7e4b7241c0c07478f8a2a
RiccardoLoPresti <riccardolopresti35@gmail.com>
2023-01-18 13:32:12 +0100

    [fix] header include

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -2,12 +2,10 @@
 
 @section('content')
 
-@include('layouts.partials.header')
-
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8 pt-5">
-            <h1>HOME</h1>
+            <h1>PAGINA PUBBLICA</h1>
         </div>
     </div>
 </div>
